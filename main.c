86b336a9243da0adaaef49af4ec04992bdd17617
dpfls0922 <dpfls0922@naver.com>
2024-01-15 15:57:49 +0900

    [Add] error 출력 메세지 함수 추가

--- main.c
+++ main.c
@@ -6,21 +6,34 @@
 /*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 15:37:09 by yerilee           #+#    #+#             */
-/*   Updated: 2024/01/15 15:55:39 by yerilee          ###   ########.fr       */
+/*   Updated: 2024/01/15 15:56:53 by yerilee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	init_game(t_game *game, char *file)
+void	error(char *s)
 {
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		write(1, &s[i], 1);
+		i++;
+	}
+	exit(0);
 }
+
+// void	init_game(t_game *game, char *file)
+// {
+// }
 
 int	main(int argc, char **argv)
 {
 	t_game	game;
 
-
+	(void)argv;
 	if (argc != 2)
 		error("Error\n");
 	// argv[1] 입력값 유효성 체크 추가하기
