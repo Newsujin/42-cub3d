e9a548c447827f14aca044165dd1583f580451be
dpfls0922 <dpfls0922@naver.com>
2024-01-15 15:47:00 +0900

    [Add] 초기 헤더파일 작성

---
+++ cub3d.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yerilee <yerilee@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/15 15:36:08 by yerilee           #+#    #+#             */
+/*   Updated: 2024/01/15 15:41:31 by yerilee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <math.h>
+# include "mlx/mlx.h"
+
+#endif
