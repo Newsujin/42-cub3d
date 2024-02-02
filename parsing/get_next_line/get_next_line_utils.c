847149075a22e53cea66e62fa0c8fde5a0f25e5c
이예린 <83818069+dpfls0922@users.noreply.github.com>
2024-02-02 21:25:50 +0900

    [Refactor] parsing에서 사용되는 strjoin 함수 get_next_line에서 제거

--- parsing/get_next_line/get_next_line_utils.c
+++ parsing/get_next_line/get_next_line_utils.c
@@ -113,32 +113,3 @@ char	*ft_strjoin(char *s1, char const *s2)
 	str[ft_strlen(s1) + i] = 0;
 	return (str);
 }
-
-char	*ft_strjoin2(char *s1, char *s2, int s2_len, int count)
-{
-	char	*temp;
-	int		i;
-
-	i = 0;
-	temp = (char *)malloc(sizeof(char) * (count + s2_len + 1));
-	if (!temp)
-	{
-		free(s1);
-		return (0);
-	}
-	while (i < count)
-	{
-		temp[i] = s1[i];
-		i++;
-	}
-	i = 0;
-	while (i < s2_len)
-	{
-		temp[count + i] = s2[i];
-		i++;
-	}
-	temp[count + i] = '\0';
-	free(s1);
-	s1 = NULL;
-	return (temp);
-}
